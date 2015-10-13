b9abc033440b3ab28c2d52e54b91227813dc5505
Aaron Ecay <aaronecay@gmail.com>
2015-10-13 21:30:48 +0100

    network.R: remove irrelevant parameters, fix plotting code

--- code/network.R
+++ code/network.R
@@ -2,15 +2,16 @@ library(igraph)
 
 net <- unique(read.delim("../data/impls.txt", sep = ";", header = FALSE))
 
-## net <- subset(net, V2 != "GSI")
+net <- subset(net, ! V2 %in% c("AGS", "AGO", "EM1", "DPN", "NTD", "OKP", "FVP", "ASF"))
+net <- subset(net, ! V1 %in% c("AGS", "AGO", "EM1", "DPN", "NTD", "OKP", "FVP", "ASF"))
 
 gr <- graph.edgelist(as.matrix(net[,c(2,1)]))
 
-layout <- layout_with_fr(g)
+layout <- layout_with_fr(gr)
 
-png("../figures/network.png", wid = 6, hei = 6, res = 300, units = "in")
+tiff("../figures/network.tif", wid = 6, hei = 6, res = 300, units = "in")
 
-plot(gr, layout = vertex.size = 9, vertex.label.cex = 0.5, edge.arrow.size = 0.25)
+plot(gr, vertex.size = 9, vertex.label.cex = 0.5, edge.arrow.size = 0.25)
 
 ## plot(gr,
 ##      layout = layout.fruchterman.reingold(gr, area = 30 * vcount(gr)^2),
